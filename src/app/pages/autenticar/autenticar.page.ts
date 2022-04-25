27eec5cca2bcb446bec900a681fd0a3f43fac67e
Joel Omar Yunga Pedraza <67283182+JoelYunga@users.noreply.github.com>
2022-04-25 13:33:43 -0500

    Changes styles autenticar

--- src/app/pages/autenticar/autenticar.page.ts
+++ src/app/pages/autenticar/autenticar.page.ts
@@ -19,12 +19,12 @@ export class AutenticarPage implements OnInit {
       {id: 1, img: 'assets/img/rock.png'},
       {id: 2, img: 'assets/img/paper.png'},
       {id: 3, img: 'assets/img/scissors.png'},
-      {id: 3, img: 'assets/img/scissors.png'},
-      {id: 3, img: 'assets/img/scissors.png'},
-      {id: 3, img: 'assets/img/scissors.png'},
-      {id: 3, img: 'assets/img/scissors.png'},
-      {id: 3, img: 'assets/img/scissors.png'},
-      {id: 3, img: 'assets/img/scissors.png'},
+      {id: 4, img: 'assets/img/scissors.png'},
+      {id: 5, img: 'assets/img/scissors.png'},
+      {id: 6, img: 'assets/img/scissors.png'},
+      {id: 7, img: 'assets/img/scissors.png'},
+      {id: 8, img: 'assets/img/scissors.png'},
+      {id: 9, img: 'assets/img/scissors.png'},
     ]
     this.result = 'Esperando jugada...';
     console.log(this.pointsUser);
