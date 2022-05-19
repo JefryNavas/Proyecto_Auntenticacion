3183078b0c8cb7168fabbdb1bc6d1eae4a74559d
Jefry Navas <jnavass@est.ups.edu.ec>
2022-05-18 22:24:47 -0500

    inicio docheck

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,24 +1,23 @@
-import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild, DoCheck } from '@angular/core';
 import { Card } from './card.blueprint';
 import { Router } from '@angular/router';
-import { IgxDialogComponent } from 'igniteui-angular';
 
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.page.html',
   styleUrls: ['./inicio.page.scss'],
 })
-export class InicioPage implements OnInit {
+export class InicioPage implements DoCheck {
   @HostListener('window:hashchange', ['$event'])
   hashChangeHandler(e) {
     window.location.hash = "dontgoback";
   }
   usuario: any
   constructor(private router: Router,) {
-    this.usuario = JSON.parse(localStorage.getItem('usuario'))
+    
   }
-  public card;
-  ngOnInit() {
+  ngDoCheck(): void {
+    this.usuario = JSON.parse(localStorage.getItem('usuario'))  
     this.card = new Card({
       buttons: ['Leer más'],
       icons: ['favorite', 'share'],
@@ -26,8 +25,10 @@ export class InicioPage implements OnInit {
       subtitle: `${this.usuario.ciudad}, ${this.usuario.direccion}`,
       title: `${this.usuario.nombres} ${this.usuario.apellidos}, ${this.usuario.edad}`
     });
+    this.usuario.nacimiento = this.usuario.nacimiento.slice(0, -14);
   }
-  logout(){
+  public card;
+  logout() {
     localStorage.clear();
     this.router.navigate(['login'])
   }
